66b0ba5926cdd777a837b1964d81d3351fdb5f75
sltalex <51757517@qq.com>
2021-06-28 11:31:36 +0800

    update

--- jd_zjb.js
+++ jd_zjb.js
@@ -16,7 +16,7 @@ const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
 
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
-let InviterPin = '/eNHdfn6fP+TFwVda3ipjWwvTFqeKBZaRG38adWABKk='; //
+let InviterPin = 'IoL8Kw8TJQBy4Q/pL55KRQ=='; //
 
 
 if (process.env.InviterPin) {
