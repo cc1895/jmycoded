047940e2e3486ee6c01f872aa54916804e082e73
sltalex <51757517@qq.com>
2021-07-01 18:12:00 +0800

    京东0元砍价

--- diy.sh
+++ diy.sh
@@ -14,14 +14,12 @@ scripts_base_url_2=https://ghproxy.com/https://github.com/Youthsongs/QuanX/blob/
 
 ##############################  作  者  脚  本  名  称  （必填）  ##############################
 # 将相应作者的脚本填写到以下变量中
-my_scripts_list_1="jd_big_winner.js jd_qmwxj.js jd_ppdz.js jd_lsj.js jd_wsdlb.js jd_zxry.js jd_zooElecsport.js jd_zjb.js jd_tyt.js jd_superBrand.js jd_SplitRedPacket.js jd_qqxing.js jd_hwsx.js jd_europeancup.js jd_ddo_pk.js jd_ddnc_farmpark.js jd_xtg_help.js"
+my_scripts_list_1="jd_kanjia3.js jd_big_winner.js jd_qmwxj.js jd_ppdz.js jd_lsj.js jd_wsdlb.js jd_zxry.js jd_zooElecsport.js jd_zjb.js jd_tyt.js jd_superBrand.js jd_SplitRedPacket.js jd_qqxing.js jd_hwsx.js jd_europeancup.js jd_ddo_pk.js jd_ddnc_farmpark.js jd_xtg_help.js"
 my_scripts_list_2="jay_member_olb.js jd_party_night.jd jd_tcl.js jd_unbind.js z_tcl_liling.js"
 
 ## 由于CDN代理无法实时更新文件内容，目前使用本人的脚本收集库以解决不能访问 Github 的问题  
 ##jd_unbind 注销会员卡
-#jd_tcl 球队赢好礼
-#jd_party_night 沸腾之夜
-#jay_member_olb 欧乐
+#jd_kanjia3  砍价
 ##############################  随  机  函  数  ##############################
 rand() {
     min=$1
